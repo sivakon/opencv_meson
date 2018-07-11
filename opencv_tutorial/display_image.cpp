633d85e8e1e438f9b357e2d7e19b3f3d4b09684f
Sivaram Konanki <sivaram.konanki@p45535.intra.rakuten.co.jp>
2018-07-11 16:58:35 +0900

    meson step by step opencv

--- opencv_tutorial/display_image.cpp
+++ opencv_tutorial/display_image.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+
 extern "C" int display_image(char* s)
 {
-    cv::Mat image;
-    image = cv::imread( s, 1 );
+    Mat image;
+    image = imread( s, 1 );
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-    cv::imshow("Display Image", image);
-    cv::waitKey(0);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
     return 0;
 }
